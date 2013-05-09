ee1b601f71cbc4842ba438e935f4d44461155ffc
吴庆健 <crazywu@126.com>
2013-05-09 15:24:13 +0800

    test

--- ex_v3/ex_v3/Base.cpp
+++ ex_v3/ex_v3/Base.cpp
@@ -64,5 +64,49 @@ void main(){
 
 	cout << "[1] Base1.ibase1 = " <<(int)pVtab[1]<<endl;
 
+	int s = sizeof(Base1)/4;
+	cout<<"[" <<s << "] Base2::_vptr->"<<endl;
+	pFun = (Fun)pVtab[s][0];
+	cout << "	[0] ";
+	pFun();
+
+	pFun = (Fun)pVtab[s][1];
+	cout << "	[1] ";
+	pFun();
+
+	pFun = (Fun)pVtab[s][2];
+	cout << "	[2] ";
+	pFun();
+
+	pFun = (Fun)pVtab[s][3];
+	cout << "	[3] ";
+	cout<<pFun<<endl;
+
+	cout<<"["<<s+1<<"] Base2.ibase2 = "<<(int)pVtab[s+1]<<endl;
+
+	s = s + sizeof(Base2)/4;
+
+	cout<<"[" <<s << "] Base3::_vptr->"<<endl;
+	pFun = (Fun)pVtab[s][0];
+	cout << "	[0] ";
+	pFun();
+
+	pFun = (Fun)pVtab[s][1];
+	cout << "	[1] ";
+	pFun();
+
+	pFun = (Fun)pVtab[s][2];
+	cout << "	[2] ";
+	pFun();
+
+	pFun = (Fun)pVtab[s][3];
+	cout << "	[3] ";
+	cout<<pFun<<endl;
+
+	s++;
+	cout<<"["<<s<<"] Base3.ibase3 = "<<(int)pVtab[s]<<endl;
+	s++;
+	cout<<"["<<s<<"] Derive.iderive = "<<(int)pVtab[s]<<endl;
+
 	system("pause");
 }
